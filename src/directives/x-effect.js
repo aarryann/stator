a6921bf0b9ec55f15de7edc0564a68c7286e283f
Vivek Pillai <aarryann@yahoo.com>
2024-12-10 17:04:38 -0500

    setting up

---
+++ src/directives/x-effect.js
@@ -0,0 +1,10 @@
+import { skipDuringClone } from '../clone';
+import { directive } from '../directives';
+import { evaluate, evaluateLater } from '../evaluator';
+
+directive(
+  'effect',
+  skipDuringClone((el, { expression }, { effect }) => {
+    effect(evaluateLater(el, expression));
+  })
+);
