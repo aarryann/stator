befe7c7c8a3de5c72993cf4630c76115c99f1e2a
Vivek Pillai <aarryann@yahoo.com>
2025-02-02 16:30:41 -0500

    test utils

--- tests/unit/directives.test.js
+++ tests/unit/directives.test.js
@@ -51,7 +51,7 @@ describe('Stator.js Directives Tests', () => {
 
   test(
     'x-data test',
-    html` <div x-data='{ "foo": "bar", "count":1 }'>
+    html`<div x-data='{ "foo": "bar", "count":1 }'>
       <div x-data='{ "baz": "goo" }'>
         <div x-data='{ "foo": baz }'>
           <span id="bazSpan" x-text="foo">1</span>
@@ -63,139 +63,132 @@ describe('Stator.js Directives Tests', () => {
       const bazSpan = get('#bazSpan');
       const barSpan = get('#barSpan');
       expect(barSpan.textContent).toBe('bar');
-      expect(get(t`#bazSpan`)).toBe('goo');
+      expect(bazSpan.textContent).toBe('goo');
     }
   );
 
-  it('x-data nesting test', () => {
-    mountHTML(`
-    <div x-data='{ "foo": "bar", "count":1 }'>
-      <div x-data='{ "baz": "goo" }'>
-        <div x-data='{ "foo": baz }'>
-          <span id="bazSpan" x-text="foo">1</span>
-        </div>
-      </div>
-      <span id="barSpan" x-text="foo">2</span>
-    </div>`);
-
-    const bazSpan = document.querySelector('#bazSpan');
-    const barSpan = document.querySelector('#barSpan');
-    expect(barSpan.textContent).toBe('bar');
-    expect(bazSpan.textContent).toBe('goo');
-  });
-
-  it('x-data initializes correctly and binds data to the DOM', () => {
-    mountHTML(`<div x-data='{ "foo": "bar" }'><span x-text="foo"></span></div>`);
+  test('x-data initializes correctly and binds data to the DOM', html`<div x-data='{ "foo": "bar" }'><span x-text="foo"></span></div>`, ({ get, t }) => {
     const span = document.querySelector('span');
     expect(span.textContent).toBe('bar');
   });
 
-  it('x-bind dynamically binds attributes', () => {
-    mountHTML(
-      `<div x-data='{ "color": "red" }'>
-         <p x-bind:style="\`color: \${color}\`">Test</p>
-       </div>`
-    );
-    const element = document.querySelector('p');
-    expect(element.style.color).toBe('red');
-  });
+  test(
+    'x-bind dynamically binds attributes',
+    html`<div x-data='{ "color": "red" }'>
+      <p x-bind:style="\`color: \${color}\`">Test</p>
+    </div>`,
+    ({ get, t }) => {
+      const element = document.querySelector('p');
+      expect(element.style.color).toBe('red');
+    }
+  );
 
-  it('test ngparser interpolation', () => {
-    mountHTML(
-      `<div x-data='{ "name": "Jack Ryan", "age": 23 }'>
-         <p x-text="\`Hello, \${name}! You are \${age} years old.\`"></p>
-       </div>`
-    );
-    const element = document.querySelector('p');
-    expect(element.textContent).toBe('Hello, Jack Ryan! You are 23 years old.');
-  });
-  it('x-show toggles element visibility', () => {
-    mountHTML(
-      `<div x-data='{ "visible": true }'>
-         <p x-show="visible">Visible</p>
-       </div>`
-    );
-    const paragraph = document.querySelector('p');
-    expect(paragraph.style.display).not.toBe('none');
-  });
+  test(
+    'test ngparser interpolation',
+    html`<div x-data='{ "name": "Jack Ryan", "age": 23 }'>
+      <p x-text="\`Hello, \${name}! You are \${age} years old.\`"></p>
+    </div>`,
+    ({ get, t }) => {
+      const element = document.querySelector('p');
+      expect(element.textContent).toBe('Hello, Jack Ryan! You are 23 years old.');
+    }
+  );
+  test(
+    'x-show toggles element visibility',
+    html`<div x-data='{ "visible": true }'>
+      <p x-show="visible">Visible</p>
+    </div>`,
+    ({ get, t }) => {
+      const paragraph = document.querySelector('p');
+      expect(paragraph.style.display).not.toBe('none');
+    }
+  );
 
-  it('x-if renders and destroys elements', () => {
-    mountHTML(
-      `<div x-data='{ "show": true }'>
-         <template x-if="show">
-           <p>Conditionally Rendered</p>
-         </template>
-       </div>`
-    );
+  test(
+    'x-if renders and destroys elements',
+    html`<div x-data='{ "show": true }'>
+      <template x-if="show">
+        <p>Conditionally Rendered</p>
+      </template>
+    </div>`,
+    ({ get, t }) => {
+      const paragraph = document.body.querySelector('p');
+      expect(paragraph).not.toBeNull();
+    }
+  );
+  test(
+    'x-init runs initialization expressions',
+    html`<div x-data='{ "foo": "bar" }' x-init='foo = "baz"'>
+      <p x-text="foo"></p>
+    </div>`,
+    ({ get, t }) => {
+      const paragraph = document.querySelector('p');
+      expect(paragraph.textContent).toBe('baz');
+    }
+  );
+  test(
+    'x-on with .prevent stops default action',
+    html`<div x-data='{ "submitted": false }'>
+      <form @submit.prevent="submitted = true">
+        <button type="submit">Submit</button>
+      </form>
+    </div>`,
+    ({ get, t }) => {
+      const button = document.querySelector('button');
+      button.click();
 
-    const paragraph = document.body.querySelector('p');
-    expect(paragraph).not.toBeNull();
-  });
-  it('x-init runs initialization expressions', () => {
-    mountHTML(
-      `<div x-data='{ "foo": "bar" }' x-init='foo = "baz"'>
-         <p x-text="foo"></p>
-       </div>`
-    );
-    const paragraph = document.querySelector('p');
-    expect(paragraph.textContent).toBe('baz');
-  });
-  it('x-on with .prevent stops default action', async () => {
-    mountHTML(
-      `<div x-data='{ "submitted": false }'>
-         <form @submit.prevent="submitted = true">
-           <button type="submit">Submit</button>
-         </form>
-       </div>`
-    );
-    const button = document.querySelector('button');
-    await document.querySelector('button').click();
+      expect(document.querySelector('[x-data]')._x_dataStack[0].submitted).toBe(true);
+    }
+  );
+  test(
+    'binds a value to a DOM element correctly',
+    html`<div x-data='{ "color": "blue" }'>
+      <p x-bind:style="'color: ' + color"></p>
+    </div>`,
+    ({ get, t }) => {
+      const element = document.querySelector('p');
+      expect(element.style.color).toBe('blue');
+    }
+  );
 
-    expect(document.querySelector('[x-data]')._x_dataStack[0].submitted).toBe(true);
-  });
-  it('binds a value to a DOM element correctly', () => {
-    mountHTML(
-      `<div x-data='{ "color": "blue" }'>
-         <p x-bind:style="'color: ' + color"></p>
-       </div>`
-    );
-    const element = document.querySelector('p');
-    expect(element.style.color).toBe('blue');
-  });
-
-  it('renders raw HTML using x-html', () => {
-    mountHTML(
-      `<div x-data='{ "rawHTML": "<span>Rendered</span>" }'>
-         <p x-html="rawHTML"></p>
-       </div>`
-    );
-    const renderedSpan = document.querySelector('p span');
-    expect(renderedSpan).not.toBeNull();
-    expect(renderedSpan.textContent).toBe('Rendered');
-  });
-  it('clicking a button to toggle visibility', async () => {
-    mountHTML(`<div x-data="{ isOpen: false }">
+  test(
+    'renders raw HTML using x-html',
+    html`<div x-data='{ "rawHTML": "<span>Rendered</span>" }'>
+      <p x-html="rawHTML"></p>
+    </div>`,
+    ({ get, t }) => {
+      const renderedSpan = document.querySelector('p span');
+      expect(renderedSpan).not.toBeNull();
+      expect(renderedSpan.textContent).toBe('Rendered');
+    }
+  );
+  test(
+    'clicking a button to toggle visibility',
+    html`<div x-data="{ isOpen: false }">
       <button @click="isOpen = !isOpen">Click Me</button>
       <span x-show="isOpen"></span>
-    </div>`);
-    expect(document.querySelector('span').style.display).toEqual('none');
-    const button = document.querySelector('button');
-    button.click();
-    await waitFor(() => {
+    </div>`,
+    ({ get, t }) => {
       expect(document.querySelector('span').style.display).toEqual('none');
-    });
-  });
+      const button = document.querySelector('button');
+      button.click();
+      waitFor(() => {
+        expect(document.querySelector('span').style.display).toEqual('none');
+      });
+    }
+  );
 
-  it('resolves DOM elements using $refs', () => {
-    mountHTML(
-      `<div x-data>
-         <button x-ref="myButton">Click Me</button>
-         <p x-text="$refs.myButton.textContent"></p>
-       </div>`
-    );
-    const paragraph = document.querySelector('p');
-    expect(paragraph.textContent).toBe('Click Me');
-  });
+  test(
+    'resolves DOM elements using $refs',
+    html`<div x-data>
+      <button x-ref="myButton">Click Me</button>
+      <p x-text="$refs.myButton.textContent"></p>
+    </div>`,
+    ({ get, t }) => {
+      const paragraph = document.querySelector('p');
+      expect(paragraph.textContent).toBe('Click Me');
+    }
+  );
 
   it('debounces function calls', async () => {
     let count = 0;
@@ -206,134 +199,139 @@ describe('Stator.js Directives Tests', () => {
     expect(count).toBe(1);
   });
 
-  it('x-for loops through arrays', () => {
-    mountHTML(
-      `<div x-data='{ "items": ["One", "Two", "Three"] }'>
-         <template x-for="item in items">
-           <p x-text="item"></p>
-         </template>
-       </div>`
-    );
-    const paragraphs = document.querySelectorAll('p');
-    expect(paragraphs.length).toBe(3);
-    expect(paragraphs[0].textContent).toBe('One');
-    expect(paragraphs[1].textContent).toBe('Two');
-    expect(paragraphs[2].textContent).toBe('Three');
-  });
+  test(
+    'x-for loops through arrays',
+    html`<div x-data='{ "items": ["One", "Two", "Three"] }'>
+      <template x-for="item in items">
+        <p x-text="item"></p>
+      </template>
+    </div>`,
+    ({ get, t }) => {
+      const paragraphs = document.querySelectorAll('p');
+      expect(paragraphs.length).toBe(3);
+      expect(paragraphs[0].textContent).toBe('One');
+      expect(paragraphs[1].textContent).toBe('Two');
+      expect(paragraphs[2].textContent).toBe('Three');
+    }
+  );
 
-  it('generates unique IDs using $id', () => {
-    mountHTML(
-      `<div x-data="{ id: $id('unique') }">
-         <p x-text="id"></p>
-       </div>`
-    );
-    const id = document.querySelector('p').textContent;
-    expect(id).toContain('unique');
-  });
+  test(
+    'generates unique IDs using $id',
+    html`<div x-data="{ id: $id('unique') }">
+      <p x-text="id"></p>
+    </div>`,
+    ({ get, t }) => {
+      const id = document.querySelector('p').textContent;
+      expect(id).toContain('unique');
+    }
+  );
 
-  it('handles complex attribute bindings', () => {
-    mountHTML(`
-        <div x-data="{ attrs: { 'data-test': 'value', class: 'test', style: 'color: red' } }">
-          <div x-bind="attrs"></div>
-        </div>
-      `);
+  test(
+    'handles complex attribute bindings',
+    html`<div x-data="{ attrs: { 'data-test': 'value', class: 'test', style: 'color: red' } }">
+      <div x-bind="attrs"></div>
+    </div> `,
+    ({ get, t }) => {
+      const div = document.querySelector('[x-bind]');
+      expect(div.dataset.test).toBe('value');
+      expect(div.classList.contains('test')).toBe(true);
+      expect(div.style.color).toBe('red');
+    }
+  );
 
-    const div = document.querySelector('[x-bind]');
-    expect(div.dataset.test).toBe('value');
-    expect(div.classList.contains('test')).toBe(true);
-    expect(div.style.color).toBe('red');
-  });
-
-  it('handles modeleable transformations', async () => {
-    mountHTML(`
-      <div x-data="{ number: 5 }">
-        <div x-data="{ count: 0 }" x-modelable="count" x-model="number">
-          <button @click="count=count+1">Increment</button>
-        </div>
-        Number: <span id="displayNumber" x-text="number"></span>
+  test(
+    'handles modeleable transformations',
+    html`<div x-data="{ number: 5 }">
+      <div x-data="{ count: 0 }" x-modelable="count" x-model="number">
+        <button @click="count=count+1">Increment</button>
       </div>
-      `);
-    const button = document.querySelector('button');
-    const span = document.querySelector('#displayNumber');
+      Number: <span id="displayNumber" x-text="number"></span>
+    </div> `,
+    ({ get, t }) => {
+      const button = document.querySelector('button');
+      const span = document.querySelector('#displayNumber');
 
-    button.click();
-    await waitFor(() => {
-      expect(span.textContent).toBe('5');
-    });
-  });
+      button.click();
+      waitFor(() => {
+        expect(span.textContent).toBe('5');
+      });
+    }
+  );
 
-  it('Nested x-for loops render correctly', () => {
-    mountHTML(
-      `<div x-data='{ "lists": [{ "items": ["A", "B"] }, { "items": ["C", "D"] }] }'>
-         <template x-for="list in lists">
-           <div>
-             <template x-for="item in list.items">
-               <p x-text="item"></p>
-             </template>
-           </div>
-         </template>
-       </div>`
-    );
+  test(
+    'Nested x-for loops render correctly',
+    html`<div x-data='{ "lists": [{ "items": ["A", "B"] }, { "items": ["C", "D"] }] }'>
+      <template x-for="list in lists">
+        <div>
+          <template x-for="item in list.items">
+            <p x-text="item"></p>
+          </template>
+        </div>
+      </template>
+    </div>`,
+    ({ get, t }) => {
+      const paragraphs = document.querySelectorAll('p');
+      expect(paragraphs.length).toBe(4);
+      expect(paragraphs[0].textContent).toBe('A');
+      expect(paragraphs[1].textContent).toBe('B');
+      expect(paragraphs[2].textContent).toBe('C');
+      expect(paragraphs[3].textContent).toBe('D');
+    }
+  );
 
-    const paragraphs = document.querySelectorAll('p');
-    expect(paragraphs.length).toBe(4);
-    expect(paragraphs[0].textContent).toBe('A');
-    expect(paragraphs[1].textContent).toBe('B');
-    expect(paragraphs[2].textContent).toBe('C');
-    expect(paragraphs[3].textContent).toBe('D');
-  });
+  test(
+    'x-on handles events',
+    html`<div x-data='{ "count": 0 }'>
+      <button x-on:click="count=count+1">Click</button>
+      <span x-text="count"></span>
+    </div>`,
+    ({ get, t }) => {
+      const button = document.querySelector('button');
+      const span = document.querySelector('span');
 
-  it('x-on handles events', async () => {
-    mountHTML(
-      `<div x-data='{ "count": 0 }'>
-         <button x-on:click="count=count+1">Click</button>
-         <span x-text="count"></span>
-       </div>`
-    );
-    const button = document.querySelector('button');
-    const span = document.querySelector('span');
+      expect(span.textContent).toBe('0');
 
-    expect(span.textContent).toBe('0');
+      button.click();
+      waitFor(() => {
+        expect(span.textContent).toBe('1');
+      });
+    }
+  );
 
-    button.click();
-    await waitFor(() => {
-      expect(span.textContent).toBe('1');
-    });
-  });
+  test(
+    'x-model two-way binds input fields',
+    html`<div x-data='{ "inputValue": "" }'>
+      <input x-model="inputValue" />
+      <p x-text="inputValue"></p>
+    </div>`,
+    ({ get, t }) => {
+      const input = document.querySelector('input');
+      const output = document.querySelector('p');
 
-  it('x-model two-way binds input fields', async () => {
-    mountHTML(
-      `<div x-data='{ "inputValue": "" }'>
-         <input x-model="inputValue">
-         <p x-text="inputValue"></p>
-       </div>`
-    );
-    const input = document.querySelector('input');
-    const output = document.querySelector('p');
+      fireEvent.update(input, 'Hello Stator!');
+      waitFor(() => {
+        expect(output.textContent).toBe('Hello Stator!');
+      });
+    }
+  );
 
-    await fireEvent.update(input, 'Hello Stator!');
-    await waitFor(() => {
-      expect(output.textContent).toBe('Hello Stator!');
-    });
-  });
+  test(
+    'x-transition applies transitions on show/hide',
+    html`<div x-data='{ "visible": false }'>
+      <p x-show="visible" x-transition>Transition Content</p>
+      <button @click="visible = !visible;">Toggle</button>
+    </div>`,
+    ({ get, t }) => {
+      const paragraph = document.querySelector('p');
+      const button = document.querySelector('button');
 
-  it('x-transition applies transitions on show/hide', async () => {
-    mountHTML(
-      `<div x-data='{ "visible": false }'>
-         <p x-show="visible" x-transition>Transition Content</p>
-         <button @click="visible = !visible;">Toggle</button>
-       </div>`
-    );
-
-    const paragraph = document.querySelector('p');
-    const button = document.querySelector('button');
-
-    expect(paragraph.style.display).toBe('none');
-    button.click();
-    await waitFor(() => {
-      expect(paragraph.style.display).not.toBe('none');
-    });
-  });
+      expect(paragraph.style.display).toBe('none');
+      button.click();
+      waitFor(() => {
+        expect(paragraph.style.display).not.toBe('none');
+      });
+    }
+  );
 
   it('handles class string transformations', async () => {
     Stator.store('test', {
